95e065ec63e5ba5b53b7c9781df588f1a681598b
muskan501 <kapoormuskan43@gmail.com>
2019-12-09 11:59:09 +0530

    Updates

---
+++ precache-manifest.24e695e7560fcffbc6da84554a3fdac2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c370635bfabd40390c9e64e81823d94",
+    "url": "/phone-directory/index.html"
+  },
+  {
+    "revision": "5248e82bae6141b95bc3",
+    "url": "/phone-directory/static/css/main.72173f80.chunk.css"
+  },
+  {
+    "revision": "380ad5db3121a85ad1f5",
+    "url": "/phone-directory/static/js/2.c3f46f5b.chunk.js"
+  },
+  {
+    "revision": "5248e82bae6141b95bc3",
+    "url": "/phone-directory/static/js/main.9a0f5c89.chunk.js"
+  },
+  {
+    "revision": "750e5236f65c3a1f5e4c",
+    "url": "/phone-directory/static/js/runtime-main.32348089.js"
+  }
+]);
